610d3188f6a8f066d6552b8cb026b55c9a42ead3
honggom <jaehongbyeon98@gmail.com>
2023-12-10 16:59:33 +0900

    :recycle: exception 리스폰스 통일

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
+from fastapi.openapi.utils import get_openapi
 from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from app.api.v1.api import api_v1_router
@@ -13,8 +15,34 @@ app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(api_v1_router)
 
 
+# openapi docs (swagger) 에서 기본으로 포함되는 422 에러를 제거하기 위한 코드
+def custom_openapi():
+    if not app.openapi_schema:
+        app.openapi_schema = get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            terms_of_service=app.terms_of_service,
+            contact=app.contact,
+            license_info=app.license_info,
+            routes=app.routes,
+            tags=app.openapi_tags,
+            servers=app.servers,
+        )
+        for _, method_item in app.openapi_schema.get("paths").items():
+            for _, param in method_item.items():
+                responses = param.get("responses")
+                if "422" in responses:
+                    del responses["422"]
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
+
 @app.exception_handler(CustomException)
-async def http_exception_handler(request: Request, exc: CustomException):
+async def custom_exception_handler(request: Request, exc: CustomException):
     return JSONResponse(
         status_code=exc.code,
         content=jsonable_encoder(
@@ -35,5 +63,15 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
                 },
                 "data": None,
             }
+        ),
+    )
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc: StarletteHTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=jsonable_encoder(
+            {"meta": {"code": exc.status_code, "message": exc.detail}, "data": None}
         ),
     )
