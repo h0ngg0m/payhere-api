91a78049076501e7229973170375ddad404335c9
honggom <jaehongbyeon98@gmail.com>
2023-12-10 16:23:11 +0900

    :recycle: api_router -> api_v1_router

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from app.api.v1.endpoint import auth, product, user
 
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
-api_router.include_router(user.router, prefix="/api/v1/users", tags=["User"])
-api_router.include_router(product.router, prefix="/api/v1/products", tags=["Product"])
+api_v1_router = APIRouter()
+api_v1_router.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
+api_v1_router.include_router(user.router, prefix="/api/v1/users", tags=["User"])
+api_v1_router.include_router(
+    product.router, prefix="/api/v1/products", tags=["Product"]
+)
--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from app.api.v1.api import api_router
+from app.api.v1.api import api_v1_router
 from app.core.config import settings
 from app.core.exception import CustomException
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
-app.include_router(api_router)
+app.include_router(api_v1_router)
 
 
 @app.exception_handler(CustomException)
